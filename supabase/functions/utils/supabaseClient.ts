0018207bc86becd61d60f6f30e8f81e358e7bf20
Chuck Shin <yishin71@gmail.com>
2025-02-11 09:52:29 +0900

    fix: 불필요 코드 리마크

--- supabase/functions/utils/supabaseClient.ts
+++ supabase/functions/utils/supabaseClient.ts
@@ -3,12 +3,10 @@ import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 let supabaseUrl = Deno.env.get("SUPABASE_URL");
 let supabaseKey = Deno.env.get("SUPABASE_SERVICE_ROLE_KEY");
 
-const isDebugMode = Deno.env.get("DEBUG_MODE") === "true";
-if (isDebugMode) {
-    supabaseUrl = "https://xgrbjyqqckbwpuyolqwu.supabase.co";
-    supabaseKey =
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhncmJqeXFxY2tid3B1eW9scXd1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTcyMTAyNDI1MSwiZXhwIjoyMDM2NjAwMjUxfQ.2SeESr_NozdlwsaUNghbw3pYiBp_eiRk_KIj2A_XVkc";
-}
+// const isDebugMode = Deno.env.get("DEBUG_MODE") === "true";
+// if (isDebugMode) {
+//     supabaseKey = Deno.env.get("DEBUG_KEY");
+// }
 
 if (!supabaseUrl || !supabaseKey) {
     console.error(
